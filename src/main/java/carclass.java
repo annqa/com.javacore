f46b467c160e1b331aca66fe168251a97f1ab31f
annqa <I2pmml4smgo>
2020-04-04 21:05:51 -0600

    methods

---
+++ src/main/java/carclass.java
@@ -0,0 +1,13 @@
+public class carclass {
+
+    public String brand;
+    public String color;
+    public int gasVolume;
+
+    public void klakson(){
+        System.out.println("beep");
+    }
+
+
+
+}
